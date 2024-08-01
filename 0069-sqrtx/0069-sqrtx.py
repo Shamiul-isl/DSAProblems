d02a470010d7dc948c253c16101158e63d21c961
Shamiul-isl <60125182+Shamiul-isl@users.noreply.github.com>
2024-08-01 00:47:29 -0400

    Time: 41 ms (51.82%), Space: 16.5 MB (18.74%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x # return x if it is 0 or 1
+
+        left, right = 2, x // 2 
+        mid = 0
+        num = 0 
+        while left <= right: # binary search for the square root
+            mid = left + (right - left) // 2 # find the middle element
+            num = mid * mid
+            if num > x:
+                right = mid - 1 # if mid * mid is greater than x, set right to mid - 1
+            elif num < x:
+                left = mid + 1 # if mid * mid is less than x, set left to mid + 1
+            else:
+                return mid # if mid * mid is equal to x, return mid
+        return right
