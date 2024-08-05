c7a0e69193b2d99654deea2fbdf6536f88be0223
Shamiul-isl <60125182+Shamiul-isl@users.noreply.github.com>
2024-08-05 17:14:03 -0400

    Time: 0 ms (100.00%), Space: 44.6 MB (75.41%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -8,6 +8,17 @@ class Solution {
         boolean changed = false;
 
         for (int j = i + 1; j < nums.length; j++) {
+          // if (!changed) {
+          //   if (nums[i] != nums[j]) 
+          //     i++;
+          //   else
+          //     changed = true;
+          // } else {
+          //   if (nums[i] != nums[j]) {
+          //     i++;
+          //     nums[i] = nums[j];
+          //   }
+          // }
             if (nums[i] != nums[j]) {
               i++;
               nums[i] = nums[j];
