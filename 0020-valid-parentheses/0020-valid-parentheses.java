4752fdf25f121af2c6495002b4db2e75ff63bb56
Shamiul-isl <60125182+Shamiul-isl@users.noreply.github.com>
2024-08-08 02:12:37 -0400

    Time: 1 ms (98.90%), Space: 41.3 MB (63.48%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        LinkedList<Character> stack = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+           char cur = s.charAt(i);
+            if (cur == '(' || cur == '[' || cur == '{'){
+                stack.push(cur);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                if (cur == ')' && stack.pop() != '(') {
+                    return false;
+                } else if (cur == ']' && stack.pop() != '[') {
+                    return false;
+                } else if (cur == '}' && stack.pop() != '{') {
+                    return false;
+                }
+            }
+        }
+        if (stack.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+}
