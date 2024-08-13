967d52963aa847d33f039b34b3958e2f1337fbde
Shamiul-isl <60125182+Shamiul-isl@users.noreply.github.com>
2024-08-12 23:23:32 -0400

    Time: 137 ms (55.60%), Space: 152.8 MB (64.46%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -3,29 +3,16 @@ class Solution {
 
     public boolean validPath(int n, int[][] edges, int start, int end) {
         // ToDo: Write Your Code Here.
-        Map<Integer, LinkedList<Integer>> neighbors = new HashMap<>();
+        Map<Integer, ArrayList<Integer>> neighbors = new HashMap<>();
         for (int i = 0; i < edges.length; i++) {
             int u = edges[i][0];
             int v = edges[i][1];
-            neighbors.computeIfAbsent(u, k -> new LinkedList<Integer>());
-            neighbors.computeIfAbsent(v, k -> new LinkedList<Integer>());
-            neighbors.get(u).push(v);
-            neighbors.get(v).push(u);
+            neighbors.computeIfAbsent(u, k -> new ArrayList<Integer>());
+            neighbors.computeIfAbsent(v, k -> new ArrayList<Integer>());
+            neighbors.get(u).add(v);
+            neighbors.get(v).add(u);
 
         }
-        
-//         List<List<Integer>> graph = new ArrayList<>();
-        
-//         // Initialize the graph
-//         for (int i = 0; i < n; i++) {
-//             graph.add(new ArrayList<>());
-//         }
-        
-//         // Populate the graph from edges
-//         for (int[] edge : edges) {
-//             graph.get(edge[0]).add(edge[1]);
-//             graph.get(edge[1]).add(edge[0]); // Because it's an undirected graph
-//         }
                 
         LinkedList<Integer> seen = new LinkedList<>();
         visited = new boolean[n];
