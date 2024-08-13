00dfcbbfd098a763af898fc17264b95920c6ef46
Shamiul-isl <60125182+Shamiul-isl@users.noreply.github.com>
2024-08-13 17:00:01 -0400

    Time: 50 ms (76.25%), Space: 51.1 MB (16.25%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
@@ -3,14 +3,14 @@
  * @return {boolean}
  */
 var checkIfPangram = function(sentence) {
-    let alphabet = new Set("abcdefghijklmnopqrstuvwxyz");
+    let alphabet = new Set(sentence);
+    return alphabet.size === 26;
+//     for (let i = 0; i < sentence.length; i++) {
+//         let cur = sentence[i].toLowerCase();
+//         if (alphabet.has(cur)) {
+//             alphabet.delete(cur);
+//         }
+//     }
     
-    for (let i = 0; i < sentence.length; i++) {
-        let cur = sentence[i].toLowerCase();
-        if (alphabet.has(cur)) {
-            alphabet.delete(cur);
-        }
-    }
-    
-    return alphabet.size === 0;
+//     return alphabet.size === 0;
 };
