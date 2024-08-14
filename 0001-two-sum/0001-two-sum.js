65ad5b86663055f36d9c3a662c9a918627048587
Shamiul-isl <60125182+Shamiul-isl@users.noreply.github.com>
2024-08-13 20:18:52 -0400

    Time: 51 ms (92.53%), Space: 51.8 MB (17.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -9,13 +9,15 @@ var twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++) {
         let complement = target - nums[i];
         if (seen.has(complement)) {
-            return [seen.get(complement)[0], i];
+            return [seen.get(complement), i];
         }
         
-        if (seen.has(nums[i])) {
-            seen.get(nums[i]).push(i);
-        } else
-            seen.set(nums[i], [i]);
+        seen.set(nums[i], i);
+        
+        // if (seen.has(nums[i])) {
+        //     seen.get(nums[i]).push(i);
+        // } else
+        //     seen.set(nums[i], [i]);
     }
     
 };
