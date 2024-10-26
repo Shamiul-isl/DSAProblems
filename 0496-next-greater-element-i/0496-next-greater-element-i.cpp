3c43c9ef636cf06137341112f8c0e8070624398f
Shamiul-isl <60125182+Shamiul-isl@users.noreply.github.com>
2024-10-26 12:35:35 -0400

    Time: 0 ms (100.00%), Space: 11.6 MB (73.52%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        vector<int> ms;
+        std::map<int, int> nums;
+
+        for (int i = 0; i < nums1.size(); i++) {
+            nums.insert({nums1.at(i), i});
+            ans.push_back(-1);
+        }
+
+        for (int i = 0; i < nums2.size(); i++) {
+            while (!ms.empty() && ms.back() < nums2[i]) {
+                if (nums.count(ms.back()) > 0) {
+                    ans.at(nums[ms.back()]) = nums2[i];
+                }
+                ms.pop_back();
+            }
+
+            ms.push_back(nums2[i]);
+        }
+            // TODO: Write your code here
+        return ans;
+    }
+};
