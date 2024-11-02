d6b2ed71ad03f04f03d8aa74ee46c23c8b5fcb9a
Shamiul-isl <60125182+Shamiul-isl@users.noreply.github.com>
2024-11-01 21:28:50 -0400

    Time: 22 ms (9.68%), Space: 24 MB (5.37%) - LeetHub

---
+++ 2363-merge-similar-items/2363-merge-similar-items.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1, vector<vector<int>>& items2) {
+        vector<vector<int>> result;
+        
+        std::set<vector<int>> seen;
+
+        for (vector<int> v: items1) {
+            seen.insert(v);
+        }
+
+        for (vector<int> v: items2) {
+            if (seen.count(v) > 0) {
+                seen.erase(v);
+                v[1] += v[1];      
+            }
+            seen.insert(v);
+        }
+
+        auto i = seen.begin();
+        cout << (*i)[0] << endl;
+        while (i != seen.end()) {
+            vector<int> cur = { (*i)[0], (*i)[1]};
+            while (true) {
+                std::advance(i, 1);
+                if (i == seen.end())
+                    break;
+                if ((*i)[0] != cur[0]) {
+                    break;
+                }
+                cur[1] += (*i)[1];
+                
+            }
+            result.push_back(cur);
+        }
+
+        
+
+        return result;
+    }
+};
