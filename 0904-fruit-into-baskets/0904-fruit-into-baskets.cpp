897e16fe6ae3369daf9133978520946698971114
Shamiul-isl <60125182+Shamiul-isl@users.noreply.github.com>
2024-11-05 14:32:29 -0500

    Time: 68 ms (15.47%), Space: 79.1 MB (6.45%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& arr) {
+        int maxLength = 0;
+        int windowStart = 0;
+        int currLength = 0;
+        map<int, int> seen;
+
+        for (int i = 0; i < arr.size(); i++) {
+            if (seen.count(arr[i]) > 0) {
+                seen[arr[i]] += 1;
+            } else {
+                while (seen.size() >= 2) {
+                    seen[arr[windowStart]] -= 1;
+
+                    if (seen[arr[windowStart]] == 0) seen.erase(arr[windowStart]);
+
+                    windowStart++;
+                    currLength--;
+                }
+
+                seen[arr[i]] = 1;
+            }
+
+            currLength++;
+
+            maxLength = max(currLength, maxLength);
+        }
+        
+        return maxLength;
+    }
+};
