f39c4b1e7f03cbb04671122c515950e52dd2b00c
Shamiul-isl <60125182+Shamiul-isl@users.noreply.github.com>
2024-11-06 14:29:06 -0500

    Time: 18 ms (21.21%), Space: 13.3 MB (5.16%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
-    int threeSumClosest(vector<int>& arr, int targetSum) {
-        int closest = NULL;
-        sort(arr.begin(), arr.end());
+  int threeSumClosest(vector<int>& arr, int targetSum) {
+    int closest = INT_MAX;
+    sort(arr.begin(), arr.end());
 
-        for (int i = 0; i < arr.size(); i++) {
-          int start = i+1, end = arr.size() - 1;
-          while (start < end) {
-            int curSum = arr[i] + arr[start] + arr[end];
-            int diff = abs(curSum - targetSum);
-            if (closest == NULL) {
-              closest = curSum;
-              if (curSum > targetSum)
-                end--;
-              else
-                start++;
-            } else {
-              int closeDiff = abs(closest - targetSum);
-              if (diff == closeDiff) {
-                closest = min(closest, curSum);
-              } else if (diff < closeDiff) {
-                closest = curSum;
-              }
-              
-              if (curSum > targetSum)
-                end--;
-              else
-                start++;  
-                
-            }
-              
-            if (closest == targetSum) return closest;
-
+    for (int i = 0; i < arr.size(); i++) {
+      int start = i+1, end = arr.size() - 1;
+      while (start < end) {
+        int curSum = arr[i] + arr[start] + arr[end];
+        int diff = abs(curSum - targetSum);
+        if (closest == INT_MAX) {
+          closest = curSum;
+          if (curSum > targetSum)
+            end--;
+          else
+            start++;
+        } else {
+          int closeDiff = abs(closest - targetSum);
+          if (diff == closeDiff) {
+            closest = min(closest, curSum);
+          } else if (diff < closeDiff) {
+            closest = curSum;
           }
+          
+          if (curSum > targetSum)
+            end--;
+          else
+            start++;  
+            
         }
-        return closest;
+          
+        if (closest == targetSum) return closest;
+
+      }
     }
+    return closest;
+  }
 };
