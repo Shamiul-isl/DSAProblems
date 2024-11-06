bedc05b701c0e8a21764df2dd2fe92c8e43e6e0e
Shamiul-isl <60125182+Shamiul-isl@users.noreply.github.com>
2024-11-06 01:54:43 -0500

    Time: 3 ms (63.43%), Space: 14.2 MB (5.06%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -4,32 +4,8 @@ public:
         stack<string> seen;
         string temp = "";
         for (int i = 0; i < path.size(); i++) {          
-//             if (path[i] == '.') {
-//                 string t =  "";
-//                 t += path[i];
-//                 int j = i + 1;
-//                 while (j < path.size()) {
-//                     if (path[j] == '.') { 
-//                         t += path[j];
-//                         j++;
-//                     } else
-//                         break;
-//                 }
-                
-                
-//                 if (t.size() == 2 && seen.size() > 1) {
-//                     if (seen.top() == "/") {
-//                         seen.pop();
-//                     }
-//                     seen.pop();
-//                 } else if (t.size() > 2) {
-//                     seen.push(t);
-//                 }
-//                 i += t.size() - 1;
-//             } else 
             
             if (path[i] == '/') {
-
                 if (temp.size() == 2 && temp == "..") {
                     if (seen.size() > 1) {
                         if (seen.top() == "/") {
