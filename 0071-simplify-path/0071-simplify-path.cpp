9106eb3b1a45c9a4a7c591f3fae19a2b9deafc97
Shamiul-isl <60125182+Shamiul-isl@users.noreply.github.com>
2024-11-06 12:21:08 -0500

    Time: 3 ms (63.43%), Space: 11.8 MB (41.25%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -2,50 +2,28 @@ class Solution {
 public:
     string simplifyPath(string path) {
         stack<string> seen;
-        string temp = "";
-        for (int i = 0; i < path.size(); i++) {          
-            
-            if (path[i] == '/') {
-                if (temp.size() == 2 && temp == "..") {
-                    if (seen.size() > 1) {
-                        if (seen.top() == "/") {
-                            seen.pop();
-                        }
-                        seen.pop();
-                    }
-                } else if (!(temp.size() == 1 && temp[0] == '.') && temp != "") {
-                    seen.push(temp);
-                }
-                temp = "";
-                if (seen.size() == 0 || (seen.size() > 0 && seen.top() != "/")) {
-                    seen.push("/");
-                }  
-            } else {
-                temp += path[i];
+        string temp;
+
+        stringstream ss(path);
+
+        while (getline(ss, temp, '/')) {
+            if (temp == "..") {
+                if (!seen.empty()) seen.pop();
+            } else if (temp != "." && temp.size() > 0) {
+                seen.push(temp);
             }
-        }
-        
-        if (temp.size() == 2 && temp == "..") {
-            if (seen.size() > 1) {
-                if (seen.top() == "/") {
-                    seen.pop();
-                }
-                seen.pop();
-            }
+
             temp = "";
         }
         
-        if (temp.size() == 1 && temp[0] == '.') temp = "";
-        
-        if (temp.size() > 0) seen.push(temp);
 
-        string res = "";
-        if (seen.top() == "/" && seen.size() > 1)
-            seen.pop();
-        while (seen.size() > 0) {
-            res = seen.top() + res;
+        while (!seen.empty()) {
+            temp = "/" + seen.top() + temp;
             seen.pop();
         }
-        return res;
+        
+        if (temp.size() == 0) return "/";
+
+        return temp;
     }
 };
