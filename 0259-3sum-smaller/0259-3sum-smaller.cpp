6e454ddfe95846914e6d2cf2c7ade5350f967118
Shamiul-isl <60125182+Shamiul-isl@users.noreply.github.com>
2024-11-07 14:47:51 -0500

    Time: 29 ms (30.53%), Space: 16.9 MB (53.42%) - LeetHub

--- 0259-3sum-smaller/0259-3sum-smaller.cpp
+++ 0259-3sum-smaller/0259-3sum-smaller.cpp
@@ -6,14 +6,16 @@ public:
 
         for (int i = 0; i < arr.size(); i++) {
           for (int start = i + 1; start < arr.size() - 1; start++) {
-            int end = arr.size() - 1;
-            if (arr[i] + arr[start] + arr[start + 1] > target) {
-                continue;
-            }
-            while (start < end) {
+            int end = start + 1;
+
+            while (end < arr.size()) {
               int cur = arr[i] + arr[start] + arr[end];
-              if (cur < target) count++;
-              end--;
+              if (cur < target) 
+                  count++;
+              else
+                  break;
+              
+              end++;
             }
           }
 
