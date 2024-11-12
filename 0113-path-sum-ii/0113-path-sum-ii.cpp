f4720773db407bd326f7089b61c3b2f40d29d8ae
Shamiul-isl <60125182+Shamiul-isl@users.noreply.github.com>
2024-11-12 13:18:16 -0500

    Time: 17 ms (14.69%), Space: 40.7 MB (6.92%) - LeetHub

---
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  vector<vector<int>> pathSum(TreeNode *root, int sum) {
+    vector<vector<int>> allPaths;
+
+    return findPath(root, vector<int>(), 0, sum);
+  }
+
+  vector<vector<int>> findPath(TreeNode *cur, vector<int> res, int curSum, int targetSum) {
+    vector<vector<int>> total;
+    if (!cur) return total;
+    // if (cur->val > targetSum) return total;
+
+    int sum = cur->val + curSum;
+    // vector<vector<int>> total;
+    res.push_back(cur->val);
+    // total.push_back(res);
+    
+    if (cur->left == nullptr && cur->right == nullptr) {
+      if (cur->val == targetSum) {
+        total.push_back(res);
+      }
+
+      return total;
+    }
+
+    if (cur->left) {
+      vector<vector<int>> l = findPath(cur->left, res, sum, targetSum - cur->val);
+      if (l.size() > 0) total.insert(total.end(), l.begin(), l.end());
+    }
+
+    if (cur->right) {
+      vector<vector<int>> r = findPath(cur->right, res, sum, targetSum - cur->val);
+      if (r.size() > 0) total.insert(total.end(), r.begin(), r.end());
+    }
+
+    return total;
+
+  }
+};
