17bd23fc3996102e9bf620b052301c943afd829c
Shamiul-isl <60125182+Shamiul-isl@users.noreply.github.com>
2024-11-13 13:50:49 -0500

    Time: 0 ms (100.00%), Space: 13.1 MB (5.77%) - LeetHub

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        int total = 0;
+        allSums(root, 0, total);
+        return total;
+      }
+
+      void allSums(TreeNode *cur, int curDigits, int &total) {
+        if (!cur) return;
+
+        int temp = stoi(to_string(curDigits) + to_string(cur->val));
+        if (!cur->left && !cur->right) {  
+          total += temp;
+        }
+
+        allSums(cur->left, temp, total);
+        allSums(cur->right, temp, total);
+      }
+};
