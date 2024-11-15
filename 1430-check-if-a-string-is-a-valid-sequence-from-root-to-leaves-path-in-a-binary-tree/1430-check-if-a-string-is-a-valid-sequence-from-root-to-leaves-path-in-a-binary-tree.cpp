b5c2cb720e7c3365f7fcd9b64e0f34be673e9002
Shamiul-isl <60125182+Shamiul-isl@users.noreply.github.com>
2024-11-15 00:09:14 -0500

    Time: 0 ms (100.00%), Space: 47.3 MB (95.45%) - LeetHub

--- 1430-check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree/1430-check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree.cpp
+++ 1430-check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree/1430-check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree.cpp
@@ -12,50 +12,48 @@
 class Solution {
 public:
     bool isValidSequence(TreeNode* root, const vector<int>& arr) {
-        bool res = findPathRecursive(root,arr, 0);
+        bool res = findPathRecursive(root, 0, arr);
         return res;
     }
 
-//     bool tryPath(TreeNode *cur, int index, const vector<int> &sequence) {
-//         if (!cur) return false;
+    bool findPathRecursive(TreeNode *cur, int index, const vector<int> &sequence) {
+        if (!cur) return false;
 
-//         if (cur->val != sequence[index]) return false;
+        if (index >= sequence.size() || cur->val != sequence[index]) return false;
 
-//         if (!cur->left && !cur->right && index == sequence.size() - 1)
-//           return true;
-//         else {
-//           bool left, right;
-//           if (cur->left && index < sequence.size() - 1) left = tryPath(cur->left, index+1, sequence);
-//           if (cur->right && index < sequence.size() - 1) right = tryPath(cur->right, index+1, sequence);
+        if (!cur->left && !cur->right && index == sequence.size() - 1) {
+          return true;
+        }
+        
+        bool left = false, right = false;
+        if (cur->left) left = findPathRecursive(cur->left, index+1, sequence);
+        if (cur->right) right = findPathRecursive(cur->right, index+1, sequence);
 
-//           if (left || right) {
-//             return true;
-//           }
-
-//           return false;
-//         }
+        return left || right;
+        
+        // return findPathRecursive(cur->left, index+1, sequence) || findPathRecursive(cur->right, index+1, sequence);
+    }
+//     bool findPathRecursive(TreeNode *currentNode, const vector<int> &sequence,
+//                                 int sequenceIndex) {
+//     if (currentNode == nullptr) {
+//       return false;
 //     }
-    bool findPathRecursive(TreeNode *currentNode, const vector<int> &sequence,
-                                int sequenceIndex) {
-    if (currentNode == nullptr) {
-      return false;
-    }
 
-    if (sequenceIndex >= sequence.size() || currentNode->val != sequence[sequenceIndex]) 
-    {
-      return false;
-    }
+//     if (sequenceIndex >= sequence.size() || currentNode->val != sequence[sequenceIndex]) 
+//     {
+//       return false;
+//     }
 
-    // if the current node is a leaf, add it is the end of the sequence, we have found 
-    // a path!
-    if (currentNode->left == nullptr && currentNode->right == nullptr &&
-        sequenceIndex == sequence.size() - 1) {
-      return true;
-    }
+//     // if the current node is a leaf, add it is the end of the sequence, we have found 
+//     // a path!
+//     if (currentNode->left == nullptr && currentNode->right == nullptr &&
+//         sequenceIndex == sequence.size() - 1) {
+//       return true;
+//     }
 
-    // recursively call to traverse the left and right sub-tree
-    // return true if any of the two recursive call return true
-    return findPathRecursive(currentNode->left, sequence, sequenceIndex + 1) ||
-          findPathRecursive(currentNode->right, sequence, sequenceIndex + 1);
-  }
+//     // recursively call to traverse the left and right sub-tree
+//     // return true if any of the two recursive call return true
+//     return findPathRecursive(currentNode->left, sequence, sequenceIndex + 1) ||
+//           findPathRecursive(currentNode->right, sequence, sequenceIndex + 1);
+//   }
 };
