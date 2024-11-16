123c06a4985107da431c75ba91884b325fd7339f
Shamiul-isl <60125182+Shamiul-isl@users.noreply.github.com>
2024-11-16 16:31:55 -0500

    Time: 1 ms (86.60%), Space: 97.2 MB (98.14%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,39 +1,20 @@
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        std::vector<int> result;
-        map<int, vector<int>> edgeMap;
-        unordered_set<int> visited;
-
-        for (int i = 0; i < edges.size(); i++) {
-            if (edgeMap.count(edges[i][0]) > 0) {
-                edgeMap[edges[i][0]].push_back(edges[i][1]);
-            } else {
-                vector<int> temp = {edges[i][1]};
-                edgeMap[edges[i][0]] = temp;
-            }
+        // List to signify if the vertex has an incoming edge or not.
+        vector<bool> isIncomingEdgeExists(n, false);
+        for (vector<int>& edge : edges) {
+            isIncomingEdgeExists[edge[1]] = true;
         }
-
+        
+        vector<int> requiredNodes;
         for (int i = 0; i < n; i++) {
-            if (visited.count(i) < 1) {
-                dfs(i, edgeMap, visited);
+            // Store all the nodes that don't have an incoming edge.
+            if (!isIncomingEdgeExists[i]) {
+                requiredNodes.push_back(i);
             }
         }
-
-        for (int i = 0; i < n; i++) {
-            if (visited.count(i) < 1) {
-                result.push_back(i);
-            }
-        }
-        return result;
-    }
-    
-    void dfs(int cur, map<int, vector<int>>& edgeMap, unordered_set<int>& visited) {
-        for (int i : edgeMap[cur]) {
-            if (visited.count(i) < 1) {
-                visited.insert(i);
-                dfs(i, edgeMap, visited);
-            }
-        }
+        
+        return requiredNodes;
     }
 };
